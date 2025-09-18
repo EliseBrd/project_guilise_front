c768352144a007d2d7e931ad9a197e3e70372f99
Elise-44 <bouardelise.perso@gmail.com>
2025-09-18 14:46:41 +0200

    ajout page login + correction navigate register

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/variables.scss";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
           <Routes>
               <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
           </Routes>
       </BrowserRouter>
   )
